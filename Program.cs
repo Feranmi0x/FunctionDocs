92ee15dc97ed6517b2b9b38b21bc9570ddcacddc
Feranmi <omodaraaduraferanmi@gmail.com>
2025-07-17 02:29:07 +0100

    Fix: use correct DbContext and ensure migration setup

--- Program.cs
+++ Program.cs
@@ -10,6 +10,12 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure middleware
 if (!app.Environment.IsDevelopment())
 {
